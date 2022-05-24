3ca3adb6892d50b07c427c5f6f62079f3e60739d
ajaydevaraj63 <ajaydvrj@gmail.com>
2022-05-24 11:32:04 +0530

    f

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
 class login extends StatelessWidget {
-  const login({ Key? key }) : super(key: key);
+  const login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-       SafeArea(child: Center(child: Text('login'))),
-      
-    );
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage("assets/images/istockphoto-1151170153-612x612.jpg"),
+          ),
+        ),Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(decoration: InputDecoration( border: OutlineInputBorder(),hintText: "username"),),
+        )
+      ,Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(  decoration: InputDecoration( border: OutlineInputBorder(),hintText: "password"),),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(10),
+        child: ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.check), label: Text("login")),
+      )
+   ],
+    )));
   }
 }
